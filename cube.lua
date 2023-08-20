f9e125f8f2551b9e647cb8823466305fb229a146
Harton <hartontw@gmail.com>
2023-08-20 17:19:50 +0200

    Created some content

---
+++ cube.lua
@@ -0,0 +1,27 @@
+local BASE_URL = "https://api.github.com/repos/IslaTortuga/"
+
+local cube = {}
+
+function cube.install()
+
+end
+
+function cube.update()
+
+end
+
+function cube.help()
+
+end
+
+function cube.version()
+
+end
+
+function cube.clean()
+    
+end
+
+function cube.search()
+
+end
---
+++ info.lua
@@ -0,0 +1,8 @@
+{
+    type = "program",
+    version = "0.0.1",
+    author = "Harton",
+    dependencies = {
+        "system"
+    }
+}
